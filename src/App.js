06e4c3acafa588dbb16df6385ca2c7975d4bae42
Sasha Sohrabi <sasha.sohrabi@valtech.com>
2022-09-15 13:55:50 +0200

    Navigation Bar Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from './pages/home/home.component';
 
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
 const App = () => {
+  const Shop = () => {
+    return <h1>I am the shop page</h1>;
+  };
 
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
