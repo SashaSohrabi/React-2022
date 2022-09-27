c4db1e552841d6340fd4596eaeb1794d85bcdf71
Sasha Sohrabi <sasha.sohrabi@valtech.com>
2022-09-27 14:31:17 +0200

    Custom Buttom Component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
+import FormInput from '../form-input/form-input.component';
+
 const defaultFormFields = {
   displayName: '',
   email: '',
@@ -50,17 +52,13 @@ const SignUpForm = () => {
     <div>
       <h1>Sign up with your email and password</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="">Display Name</label>
-        <input type="text" required onChange={handleChange} name="displayName" value={displayName} />
+        <FormInput label="Display Name" type="text" required onChange={handleChange} name="displayName" value={displayName} />
 
-        <label htmlFor="">Email</label>
-        <input type="email" required onChange={handleChange} name="email" value={email} />
+        <FormInput label="Email" type="email" required onChange={handleChange} name="email" value={email} />
 
-        <label htmlFor="">Password</label>
-        <input type="password" required onChange={handleChange} name="password" value={password} />
+        <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password} />
 
-        <label htmlFor="">Confirm Password</label>
-        <input type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword} />
+        <FormInput label="Confirm Password" type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword} />
         <button type="submit">Sign Up</button>
       </form>
     </div>
